1fe3dbb32cd0db7a58680c668fcce3768c2b739e
Vitor Menezes <161902126+Vitorram@users.noreply.github.com>
2025-11-04 18:41:45 +0000

    aula 06 - json

---
+++ aula06.py
@@ -0,0 +1,20 @@
+import json
+
+# pessoa = {
+#     'nome': 'Luiz Otávio 2',
+#     'sobrenome': 'Miranda',
+#     'enderecos': [
+#         {'rua': 'R1', 'numero': 32},
+#         {'rua': 'R2', 'numero': 55},
+#     ],
+#     'altura': 1.8,
+#     'numeros_preferidos': (2, 4, 6, 8, 10),
+#     'dev': True,
+#     'nada': None,
+# }
+
+
+with open('aula06.json', 'r')as a:
+    pessoa = json.load(a)
+    print(pessoa['nome'])
+
